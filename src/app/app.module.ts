6aa73445b7cfdcf40233d87cc80b43d9d9af63e9
VasGorn <vas.goroshko@gmail.com>
2022-11-10 07:45:32 +0200

    02-009: install store devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {StoreModule} from "@ngrx/store";
 
 import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from "./app.component";
 import {AuthModule} from "./auth/auth.module";
-import { StoreModule } from '@ngrx/store';
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, StoreModule.forRoot({}, {})],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
