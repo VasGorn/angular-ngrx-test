dc796233179591f364df04f20de0ef4cc0bc7b84
VasGorn <vas.goroshko@gmail.com>
2022-11-20 12:26:07 +0200

    02-015: update reducers of authorization

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -1,27 +1,34 @@
-import { Injectable } from "@angular/core";
-import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { catchError, exhaustMap, map, mergeMap, of, switchMap } from "rxjs";
-import { CurrentUserInterface } from "src/app/shared/types/currentUser.interface";
-import { AuthService } from "../../services/auth.service";
-import { RegisterRequestInterface } from "../../types/registerRequest.interface";
-import { registerAction, registerFailureAction, registerSuccessAction } from "../actions/register.action";
+import {HttpErrorResponse} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Actions, createEffect, ofType} from "@ngrx/effects";
+import {catchError, map, of, switchMap} from "rxjs";
+import {CurrentUserInterface} from "src/app/shared/types/currentUser.interface";
+import {AuthService} from "../../services/auth.service";
+import {
+  registerAction,
+  registerFailureAction,
+  registerSuccessAction,
+} from "../actions/register.action";
 
 @Injectable()
 export class RegisterEffect {
-    register$ = createEffect(() => this.actions$.pipe(
-        ofType(registerAction),
-        switchMap(({request}) => {
-            return this.authService.register(request).pipe(
-                map((currentUser: CurrentUserInterface) => {
-                    return registerSuccessAction({currentUser})
-                }),
-                catchError(() => {
-                    return of(registerFailureAction())
-                })
-            )
-        })
-    ))
+  register$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(registerAction),
+      switchMap(({request}) => {
+        return this.authService.register(request).pipe(
+          map((currentUser: CurrentUserInterface) => {
+            return registerSuccessAction({currentUser: currentUser});
+          }),
+          catchError((errorResponse: HttpErrorResponse) => {
+            return of(
+              registerFailureAction({errors: errorResponse.error.errors})
+            );
+          })
+        );
+      })
+    )
+  );
 
-    constructor(private actions$: Actions, 
-        private authService: AuthService) {}
+  constructor(private actions$: Actions, private authService: AuthService) {}
 }
