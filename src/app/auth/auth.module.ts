9e12bf07e207a0f4b77593e80da56d50f99932c4
VasGorn <vas.goroshko@gmail.com>
2022-12-04 15:30:00 +0200

    03-019: create the login component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -12,11 +12,16 @@ import {reducers} from "./store/reducers";
 import {BackendErrorMessagesModule} from "../shared/modules/backend-error-messages/BackendErrorMessages.module";
 import {PersistenceService} from "../shared/services/Persistence.service";
 import {LoginEffect} from "./store/effects/login.effect";
+import {LoginComponent} from "./components/login/login.component";
 
 const routes: Routes = [
   {
     path: "register",
     component: RegisterComponent,
+  },
+  {
+    path: "login",
+    component: LoginComponent,
   },
 ];
 
@@ -29,7 +34,7 @@ const routes: Routes = [
     EffectsModule.forFeature([RegisterEffect, LoginEffect]),
     BackendErrorMessagesModule,
   ],
-  declarations: [RegisterComponent],
+  declarations: [RegisterComponent, LoginComponent],
   providers: [AuthService, PersistenceService],
 })
 export class AuthModule {}
